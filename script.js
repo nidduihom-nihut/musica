2d7ae36938f2f0e2c8e56b1f5dc32cc08a402be7
tuhmo <78699189+tuhmo@users.noreply.github.com>
2021-05-29 00:10:58 +0600

    musica a music app for tasty people

---
+++ script.js
@@ -0,0 +1,55 @@
+let userDb = [
+    {
+        username: "tuhin",
+        password: "1234",
+    },
+
+    {
+        username: "tuhmo",
+        password: "4567"
+    },
+
+];
+
+
+let username = document.querySelector("#username");
+let password = document.querySelector("#password");
+let login = document.querySelector("#login");
+let status;
+
+
+login.addEventListener('click', function log(){
+    if(cheack() && username.value === "tuhin"){
+        window.open('tuhin.html')
+    }else if(cheack() && username.value == "tuhmo"){
+        window.open('tuhmo.html')
+    }
+    else{
+        console.log("false")
+    }
+})
+
+
+
+function cheack(){
+    for (let i = 0; i < userDb.length; i++) {
+        if(userDb[i].username === username.value && userDb[i].password === password.value){
+            return true
+        }
+    }
+    return false
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
